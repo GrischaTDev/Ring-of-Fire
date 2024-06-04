6e75d00bad8f59f664202bf7ebb1a53fe4252380
Grischa <Grischa.Taenzer@gmail.com>
2024-06-04 20:12:14 +0200

    Add players and install material design

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Junus', 'Manuel', 'Grischa'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
