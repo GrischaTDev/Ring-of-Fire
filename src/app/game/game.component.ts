99379e9b025e373e89f1cb4d738cfd7ab015b1ef
Grischa <Grischa.Taenzer@gmail.com>
2024-06-04 21:52:00 +0200

    Add player function with material design

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent],
+  imports: [
+    CommonModule, 
+    PlayerComponent, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -16,7 +34,7 @@ export class GameComponent {
   currentCard: string = '';
   game: Game;
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.game = new Game();
     console.log(this.game);
   }
@@ -33,5 +51,14 @@ export class GameComponent {
         this.pickCardAnimation = false;
       }, 1000)
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+  
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+      console.log('Add player', name);
+    });
   }
 }
